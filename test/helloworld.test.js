86790e3f20d29336b992ef929c9ed9308ac01271
Tadeo Santanau <san18047@byui.edu>
2023-10-16 15:34:40 -0600

    Update test function

---
+++ test/helloworld.test.js
@@ -0,0 +1,10 @@
+import sayHello from '../utils/helloworld.js';
+import assert from 'assert';
+
+it("Tests Hello World", ()=>{
+    const hello = sayHello();
+
+    assert.equal(hello, "hello");
+
+});
+
